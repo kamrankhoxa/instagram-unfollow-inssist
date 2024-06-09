fc250b8f02ec5261135d5afd4fa5d9df39928c55
Kamran Fareed <k2455552@gmail.com>
2024-06-09 13:07:31 +0500

    Updating durations and optimize script

--- script.js
+++ script.js
@@ -17,21 +17,21 @@
 // 2. Click on "FOLLOWING" on your Instagram profile.
 // 3. Install Tampermonkey and create a new script.
 // 4. Copy the code below and paste it into the Tampermonkey script editor.
-// 5. The script will run if the tab is navigated away from, minimized, or unfocused. 
+// 5. The script will run if the tab is navigated away from, minimized, or unfocused.
 // It is recommended to open a new Chrome window or push the tab to the side and let the script run in the background.
 
 (async () => {
   // Customize these variables as needed
-  const UNFOLLOW_LIMIT = 800;
-  const BREAK_DURATION = 1 * 60 * 1000; // 1 minute break
-  const TOTAL_DURATION = 3 * 60 * 1000; // 3 minutes total duration
-  const MIN_UNFOLLOW_INTERVAL = 5000; // Minimum interval of 5 seconds
-  const MAX_UNFOLLOW_INTERVAL = 15000; // Maximum interval of 15 seconds
+  const UNFOLLOW_LIMIT = 5000;
+  const BREAK_DURATION = 0.5 * 60 * 1000; // 1 minute break
+  const TOTAL_DURATION = 0.3 * 60 * 1000; // 3 minutes total duration
+  const MIN_UNFOLLOW_INTERVAL = 3000; // Minimum interval of 5 seconds
+  const MAX_UNFOLLOW_INTERVAL = 7000; // Maximum interval of 15 seconds
 
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   // Find target button by text
-  const findButton = (txt) => 
+  const findButton = (txt) =>
     [...document.querySelectorAll("button")].find(btn => btn.innerText === txt);
 
   console.log("Starting unfollow script...");
